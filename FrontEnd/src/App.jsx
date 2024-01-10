84867ab08075f9454f60eab78c12ca0afdb1613c
Wiz <im.not.ali@outlook.com>
2024-01-10 23:50:02 +0330

    Configure Project

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
+import Divar from './app/DivarQuery'
+import DivarPage from './components/templates/DivarPage'
+import Jabama from './app/JabamaQuery'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+        <Divar/>
+    <Jabama/>
     </>
+
   )
 }
 
