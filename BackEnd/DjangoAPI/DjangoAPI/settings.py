4da535539b4cb1a64b8c0074485b7659b998bbb4
Ali Shahraki <im.not.ali@outlook.com>
2024-01-20 00:54:05 +0330

    Update settings.py

--- BackEnd/DjangoAPI/DjangoAPI/settings.py
+++ BackEnd/DjangoAPI/DjangoAPI/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'divar',
         'USER': 'root',
-        'PASSWORD': '3425362@liAli',
+        'PASSWORD': '*****',
         'HOST': 'localhost',
         'PORT':'3306'
     }
